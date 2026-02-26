913ef250d431db3651e6a2314291bac3e839d564
Alphonse <cashmoov@cashmoov.net>
2026-02-26 11:57:18 +0000

    Removed rbac from client entity

--- src/models/role.py
+++ src/models/role.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, Integer, Date, func
 from src.core.database import Base
 from src.models.users import User
-from src.models.rbac_assiciation import role_permissions, user_roles, posuser_roles, client_roles
+from src.models.rbac_assiciation import role_permissions, user_roles, posuser_roles
 from sqlalchemy.orm import relationship
 
 
@@ -20,11 +20,6 @@ class Role(Base):
         secondary=user_roles,
         back_populates="roles"
     )
-    # clients = relationship(
-    #     "Client",
-    #     secondary=client_roles,
-    #     back_populates="roles"
-    # )
     posusers = relationship(
         "POSUser",
         secondary=posuser_roles,
