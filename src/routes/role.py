2c8851a0363f3c7fb148d2792a23658a6546ae5f
Alphonse <cashmoov@cashmoov.net>
2026-05-18 11:46:48 +0000

    role in prod

--- src/routes/role.py
+++ src/routes/role.py
@@ -29,7 +29,7 @@ class EntityType(str, Enum):
 def create_new_role(
     role: RoleCreate, 
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.CREATE_ROLE))
+    current_user = Depends(require_permission(Permissions.CREATE_ROLE))
 ):
     return create_role(db, role)
 
@@ -39,7 +39,7 @@ def assing_roles_to_entity(
         entity_id: int,
         role_ids: List[int],
         db: Session = Depends(get_db),
-        # current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
+        current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
 ):
     return assign_roles_to_entity(db, entity_type, entity_id, role_ids)
 
@@ -48,7 +48,7 @@ def get_entity_role(
     entity_type: EntityType,
     entity_id: int,
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.READ_ROLE))
+    current_user = Depends(require_permission(Permissions.READ_ROLE))
 ):
     return get_entity_roles(db, entity_type, entity_id)
 
@@ -57,7 +57,7 @@ def update_role(
     role_id: int, 
     data: RoleUpdate, 
     db: Session = Depends(get_db), 
-    # current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
+    current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
 ):
     updated_role = update_role(db, role_id, data)
     return updated_role
@@ -66,14 +66,14 @@ def update_role(
 def get_role(
     role_id: int, 
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.READ_ROLE))
+    current_user = Depends(require_permission(Permissions.READ_ROLE))
 ):
     return get_role_by_id(db, role_id)
 
 @role_router.get("/roles/", response_model=List[RoleResponse])
 def get_roles(
     db: Session = Depends(get_db), 
-    # current_user = Depends(require_permission(Permissions.READ_ROLE))
+    current_user = Depends(require_permission(Permissions.READ_ROLE))
 ):
     return get_all_roles(db)
 
@@ -81,7 +81,7 @@ def get_roles(
 def delete_user_role(
     role_id: int,
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.DELETE_ROLE))
+    current_user = Depends(require_permission(Permissions.DELETE_ROLE))
 ):
     if not delete_role(db, role_id):
         raise HTTPException(
@@ -97,7 +97,7 @@ def assign_permissions(
     role_id: int,
     data: RolePermissionAssign,
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
+    current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
 ):
     return assign_permissions_to_role(
         db=db,
@@ -113,7 +113,7 @@ def remove_permissions(
     role_id: int,
     data: RolePermissionAssign,
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
+    current_user = Depends(require_permission(Permissions.UPDATE_ROLE))
 ):
     role = db.query(Role).filter_by(id=role_id).first()
 
@@ -145,7 +145,7 @@ def remove_permissions(
 @role_router.get("/permissions", response_model=List[PermissionResponse])
 def list_permissions(
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.READ_ROLE))
+    current_user = Depends(require_permission(Permissions.READ_ROLE))
 ):
     return db.query(Permission).all()
 
@@ -156,7 +156,7 @@ def list_permissions(
 def get_role_permissions(
     role_id: int,
     db: Session = Depends(get_db),
-    # current_user = Depends(require_permission(Permissions.READ_ROLE))
+    current_user = Depends(require_permission(Permissions.READ_ROLE))
 ):
     role = db.query(Role).filter_by(id=role_id).first()
 
